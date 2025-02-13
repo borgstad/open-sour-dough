3f5eff41dc9a438b21abd270f89c279a18eff864
Andreas Borgstad <aborgstad@gmail.com>
2025-02-13 16:55:05 +0100

    refactor: remove dependencies

--- open_sourdough_cam/main.py
+++ open_sourdough_cam/main.py
@@ -1,8 +1,7 @@
 import time
 from pathlib import Path
+import os
 
-import cv2
-import numpy as np
 
 from open_sourdough_cam import log, settings
 
@@ -18,16 +17,6 @@ def start_monitoring_session() -> None:
     to the designated directory, and logs the actions. It operates indefinitely until interrupted.
     """
     while True:
-        video_source = str(settings.OPEN_SOURDOUGH_VIDEO_DIR)
-        camera = cv2.VideoCapture(video_source)
-        success, frame = camera.read()
-        camera.release()
-
-        if not success:
-            logger.error("Failed to capture image from camera.")
-            time.sleep(settings.OPEN_SOURDOUGH_INTERVAL)
-            continue
-
         image_dir = settings.OPEN_SOURDOUGH_ROOT_IMAGE_DIR
         image_dir.mkdir(parents=True, exist_ok=True)
 
@@ -35,13 +24,13 @@ def start_monitoring_session() -> None:
         image_filename = f"{timestamp}.jpg"
         image_path = image_dir / image_filename
 
-        save_image(image_path, frame)
+        capture_image(image_path)
         logger.info(f"Captured image: {image_path}")
 
         time.sleep(settings.OPEN_SOURDOUGH_INTERVAL)
 
 
-def save_image(image_path: Path, image: np.ndarray) -> None:
+def capture_image(image_path: Path) -> None:
     """
     Saves the provided image to the specified filesystem path.
 
@@ -49,9 +38,7 @@ def save_image(image_path: Path, image: np.ndarray) -> None:
         image_path (Path): The absolute path where the image will be saved.
         image (np.ndarray): The image data to be saved.
     """
-    success = cv2.imwrite(str(image_path), image)
-    if not success:
-        logger.error(f"Failed to save image to {image_path}")
+    os.system(f"ffmpeg -f video4linux2 -i /dev/video0 -frames:v 1 {image_path} > /dev/null 2>&1")
 
 
 if __name__ == "__main__":
